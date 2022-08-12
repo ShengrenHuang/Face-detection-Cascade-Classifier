62ec957f18de8eb54fae618672ae9921c78c354a
ShengrenHuang <108604868+ShengrenHuang@users.noreply.github.com>
2022-08-12 11:10:22 +0800

    Update face_detection (stream).py

--- face_detection (stream).py
+++ face_detection (stream).py
@@ -7,7 +7,7 @@ cap = cv2.VideoCapture(0)
 while(True):
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 9)
 
     for (x,y,w,h) in faces:
         cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 3)
